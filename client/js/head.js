6db2460b0540c45d9adff3dcd415a2b3bd149af1
Eugene Chechurin <e.chechurin@gmail.com>
2012-07-06 09:48:53 +0300

    templates for head in separate file

---
+++ client/js/head.js
@@ -0,0 +1,10 @@
+Template.head.events = {
+    "click [role=menu-item]": function(e) {
+        var el =  $(e.target);
+
+        Router.setPage(el.attr("href"), el.data("page"));
+
+        Session.set("signal", null)   
+        e.preventDefault();
+    }
+}
