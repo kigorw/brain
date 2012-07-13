4c1105689d254b4acfe99b8649a95cede4a315f8
Eugene Chechurin <e.chechurin@gmail.com>
2012-07-13 12:40:50 +0300

    autocomplete in users field

--- client/js/signal_add.js
+++ client/js/signal_add.js
@@ -1,10 +1,22 @@
+Template.signal_add.users = function() {
+    console.log(Meteor.users.find())
+    return Meteor.users.find().fetch()
+}
+
+Template.signal_add.on_load = function() {
+    Meteor.defer(function () {
+        $('#recipients').chosen()
+    })
+    return
+}
+
 Template.signal_add.events = {
     'click button': function(e) {
         var user = Meteor.user(),
             signal = {
             title: $('input[name=title]').val(),
             url:  $('input[name=url]').val(),
-            users:  $('input[name=users]').val().split(','),
+            users:  $('select#recipients').val(),
             email: user.emails,
             text: $('textarea[name=body]').val(),
             tags: [],
--- client/js/signals.js
+++ client/js/signals.js
@@ -25,31 +25,10 @@ Template.signal.signal = function() {
 	return signal 
 }
 
-
-
 Template.signal_filter.events = {
     "click a": function(e) {
         var type = e.target.dataset.type
         Router.setSignalFilter(type, true);
         e.preventDefault();
-    }
-
-}
-
-Template.signal_add.events = {
-    'click button': function(e) {
-        var user = Meteor.user(),
-            signal = {
-            title: $('input[name=title]').val(),
-            url:  $('input[name=url]').val(),
-            users:  $('input[name=users]').val().split(','),
-            email: user.emails,
-            text: $('textarea[name=body]').val(),
-            tags: [],
-            date: (new Date()).getTime(),
-            user: user.username
-        }
-        console.log("insert signal", signal)
-        Signals.insert(signal)
     }
 }
